45224fc8272e4164d809e5ab1fd692dd78b0f61c
Jullia Santos Wanderlei <144744092+WanderleiJullia@users.noreply.github.com>
2024-04-05 21:03:39 -0300

    Add files via upload

---
+++ Exercicio 6 - CSS/Formatacao.cs
@@ -0,0 +1,39 @@
+* {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font-family: Arial;
+    text-decoration: none;
+    background-color: gainsboro;
+}
+
+aside {
+    float: left;
+    position: absolute;
+    width: 20%;
+    height: 100%;
+    background-color: cornflowerblue;
+    color: white;
+}
+
+a {
+    background-color: cornflowerblue;
+    color: white;
+    margin: 30px;
+}
+
+a:hover {
+    color: gray;
+}
+
+main {
+    text-align: center;
+    float: right;
+    width: 80%;
+    height: 100%;
+}
+
+h4 {
+    font-size: 20px;
+}
---
+++ Exercicio 6 - CSS/Formatacao2.cs
@@ -0,0 +1,65 @@
+* {
+    margin: 0;
+    text-decoration: none;
+}
+
+.container {
+    width: 100%;
+    padding-top: 0;
+}
+
+.principal {
+    width: 100%;
+    height: 100%;
+    margin-top: 60px;
+}
+
+nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 40px;
+    position: fixed;
+    width: 100%;
+    height: 50px;
+    background-color: gray;
+    font-size: 20px;
+    color: white;
+    top: 0;
+}
+
+aside {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    align-items: center;
+    gap: 40px;
+    float: left;
+    position: fixed;
+    padding: 20px;
+    width: 20%;
+    height: 100%;
+    background-color: gray;
+    font-size: 20px;
+    color: white;
+}
+
+main {
+    margin-left: 320px;
+    width: 75%;
+    height: 100%;
+}
+
+footer {
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    background-color: gray;
+    color: white;
+}
+
+h2 {
+    font-size: 40px;
+    font-family: Arial;
+}
